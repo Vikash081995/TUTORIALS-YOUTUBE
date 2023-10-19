45db06f6926a426eb8849848ce824fe7d922206f
Vikash081995 <Vikash08.Kumar1995@gmail.com>
2023-10-19 18:36:35 +0530

    completed basic redux tutorials

---
+++ Codevolution/React-Redux/index.js
@@ -0,0 +1,63 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+const reduxLogger = require("redux-logger");
+
+const logger = reduxLogger.createLogger();
+
+const BUY_ICECREAM = "BUY_ICECREAM";
+const BUY_CAKE = "BUY_CAKE";
+
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "FIRST REDUX ACTION"
+  };
+}
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM,
+    info: "FIRST REDUX ACTION"
+  };
+}
+
+const initialCakeState = {
+  numOfCakes: 10
+};
+
+const initialIceCreamState = {
+  numOfIceCreams: 20
+};
+
+const cakeReducer = (state = initialCakeState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1
+      };
+
+    default:
+      return state;
+  }
+};
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams - 1
+      };
+    default:
+      return state;
+  }
+};
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(logger));
+console.log("Initial state", store.getState());
+const unsubscribe = store.subscribe(() => {});
+store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+unsubscribe();
